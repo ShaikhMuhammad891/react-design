8e4d906906dcf8cc07bbdc74f5db12786e930f5f
saifi1212 <safiullah123fix2@gmail.com>
2024-04-05 15:38:17 +0500

    brands logo

--- src/main/Trust.jsx
+++ src/main/Trust.jsx
@@ -1,5 +1,11 @@
 import React from "react";
-import { ArrowForward, Eclips, Line } from "../assets/Logos";
+import {
+  ArrowBack,
+  ArrowForward,
+  Eclips,
+  Line,
+  Smartpath,
+} from "../assets/Logos";
 import "./Services.css";
 
 const Trust = () => {
@@ -9,7 +15,7 @@ const Trust = () => {
         <div className=" ml-[105px] top-[-35.5px]">
           <Eclips />
           <div className="flex mt-[149px] gap-[96px]">
-            <div className=" text-area w-[535px] h-[420px] ">
+            <div className=" text-area max-w-[535px] ">
               <Line />
               <p className=" text-[35px] text-[#1A202C] leading-[55px] font-inter font-[400] mt-[20px]">
                 Leading companies trust us{" "}
@@ -31,11 +37,15 @@ const Trust = () => {
                 <p className=" text-[#57007B] text-[16px] font-inter font-[500] leading-[25px]">
                   See more Informations
                 </p>
-                <ArrowForward />
+                <ArrowForward color="#57007B" />
               </div>
             </div>
-            <div className=" video-section relative">
-              <img src="/images/video-image.png" alt="video" />
+            <div className=" video-section relative ">
+              <img
+                src="/images/video-image.png"
+                alt="video"
+                className=" w-[601px]"
+              />
               <img
                 src="./images/play-button.png"
                 alt="play"
@@ -44,17 +54,37 @@ const Trust = () => {
             </div>
           </div>
         </div>
-        <div className=" mt-[201px] ml-[105px] w-[412px]">
+        <div className=" mt-[201px] ml-[105px] w-[412px] ">
           <Line />
           <p className=" text-[35px] leading-[55px] font-inter font-[400] text-[#1A202C] mt-[20px]">
             Meet the People
-            <span className=" font-[700]"> <br />We are Working With</span>
+            <span className=" font-[700]">
+              {" "}
+              <br />
+              We are Working With
+            </span>
           </p>
         </div>
-        <div className=" flex gap-[15px]">
-
+        <div className=" flex gap-[22px] justify-end mr-[81px] mb-[32px]">
+          <div className=" py-[15.5px] px-[15px] border-[2px] border-[#57007B] rounded-full">
+            <ArrowBack />
+          </div>
+          <div className=" py-[15.5px] px-[15px] bg-[#57007B]  rounded-full">
+            <ArrowForward color="white" />
+          </div>
         </div>
-        
+
+        <div className=" absolute right-[553px] bottom-[-65px]">
+          <Eclips />
+        </div>
+      </div>
+      <div className=" bg-[#F7F7FA]  relative flex gap-x-[54px] items-center py-[60px] border border-y-[#E7DAED]">
+        <img src="/images/smartpath.png" alt="" />
+        <img src="/images/logo5.png" alt="" />
+        <img src="/images/Logo-6.png" alt="" />
+        <img src="/images/Logo-7.png" alt="" />
+        <img src="/images/logo8.png" alt="" />
+        <img src="/images/logo9.png" alt="" />
       </div>
     </>
   );
